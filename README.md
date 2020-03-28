4213da9285bdcd4c3a9a6af0215cac94a19aff6f
Eric Delord <edelord@gmail.com>
2020-03-28 19:45:24 +0100

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# ofp2text4scriptable
+
+## Building
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+To create a development build:
+
+```sh
+npm run build-dev
+```
+
+## Running
+
+TODO
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      inject: false,
+      appMountId: 'app',
+      filename: 'index.html'
+    })
+  ]
+};
+
+module.exports = config;
